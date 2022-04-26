49a526f36ccd4c9c4e11b58f5389244382564b0a
chinsan <clim1994@gmail.com>
2022-04-26 15:41:21 -0700

    heroku push

--- db/seed.js
+++ db/seed.js
@@ -3,16 +3,12 @@ const mongoose = require('mongoose');
 const Disc = require('../models/Disc');
 
 // Require our Bookmarks seeds
-const DiscSeed = require('../db/disc.json');
+const DiscSeed = require('./disc.json');
 
 // Logic to empty database
 
-Disc.deleteMany({})
-	.then(() => {
-		console.log('Deleted all the discs!');
-		return DiscSeed.map((disc) => {
-			return { ...disc };
-		});
+	DiscSeed.map((disc) => {
+		return { ...disc };
 	})
 	.then((disc) => {
 		return Disc.insertMany(disc);
@@ -24,3 +20,21 @@ Disc.deleteMany({})
 	.finally(() => {
 		process.exit();
 	});
+
+// Disc.deleteMany({})
+// 	.then(() => {
+// 		console.log('Deleted all the discs!');
+// 		return DiscSeed.map((disc) => {
+// 			return { ...disc };
+// 		});
+// 	})
+// 	.then((disc) => {
+// 		return Disc.insertMany(disc);
+// 	})
+// 	.then((newDisc) => {
+// 		console.log('Created new discs!', newDisc);
+// 	})
+// 	.catch(console.error)
+// 	.finally(() => {
+// 		process.exit();
+// 	});
